c2ab1cd3ee413b03fc49e39487306d393b0fbd2c
lmx-pl <it@lumex.ink>
2024-09-26 21:07:17 +0300

    Update act.js

--- act.js
+++ act.js
@@ -12,36 +12,49 @@
         'i'
       );
 
-  function replace() {
-    var frames = document.body ? document.body.getElementsByTagName('iframe') : [];
-    for (var i = 0; i < frames.length; i++) {
-      var f = frames[i];
-      var src = f.src || (f.dataset && f.dataset.src);
-      if (src && !actualRe.test(src)) {
-        var old = src.match(re);
-        if (old) {
-          var protocol = old[1] || 'https://';
-          var newDomain = actual.replace(/^https?:\/\//, '');
-          var path = old[4] || '';
-          var newSrc = protocol + newDomain + path;
-          if (f.src) {
-            f.src = newSrc;
-          }
-          if (f.dataset && f.dataset.src) {
-            f.dataset.src = newSrc;
-          }
+  function replaceFrameSrc(f) {
+    var src = f.src || (f.dataset && f.dataset.src);
+    if (src && !actualRe.test(src)) {
+      var old = src.match(re);
+      if (old) {
+        var protocol = old[1] || 'https://';
+        var newDomain = actual.replace(/^https?:\/\//, '');
+        var path = old[4] || '';
+        var newSrc = protocol + newDomain + path;
+        if (f.src) {
+          f.src = newSrc;
+        }
+        if (f.dataset && f.dataset.src) {
+          f.dataset.src = newSrc;
         }
       }
     }
   }
 
-  function init() {
-    replace();
-    var observer = new MutationObserver(replace);
-    observer.observe(document.body, { childList: true, subtree: true });
-    setInterval(replace, 10000);
+  function replace() {
+    var frames = document.body ? document.body.getElementsByTagName('iframe') : [];
+    for (var i = 0; i < frames.length; i++) {
+      replaceFrameSrc(frames[i]);
+    }
   }
 
+  function init() {
+    replace();
+    
+    var observer = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        mutation.addedNodes.forEach(function (node) {
+          if (node.tagName === 'IFRAME') {
+            replaceFrameSrc(node);
+          }
+        });
+      });
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+  }
+
+ 
   if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', init);
   } else {
