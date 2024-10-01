0bfc584c07b38c50f1e751082160b3ccde80255c
lmx-pl <it@lumex.ink>
2024-10-01 11:20:10 +0300

    remove logs & improvement some code

--- act.js
+++ act.js
@@ -5,62 +5,58 @@
   }
   window[SCRIPT_ID] = true;
 
-  var actual = 'https://p.lumex.bar',
-    re = /^(https?:\/\/)?([\w-]+\.)?lumex\.(ink|me|bar)(\/.*)?$/i,
-    actualRe = new RegExp(
-      '^' + actual.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + '(/.*)?$',
-      'i'
-    );
+  var actual = 'https://p.lumex.bar';
+  var re = /^(https?:\/\/)?([\w-]+\.)?lumex\.(ink|me|bar)(\/.*)?$/i;
+  var actualRe = new RegExp(
+    '^' + actual.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + '(/.*)?$',
+    'i'
+  );
 
-  function replaceFrameSrc(f) {
-    var src = f.src || (f.dataset && f.dataset.src);
+  function replaceFrameSrc(frame) {
+    var src = frame.src || (frame.dataset && frame.dataset.src);
     if (src && !actualRe.test(src)) {
-      console.log('replaceFrameSrc');
-      var old = src.match(re);
-      if (old) {
-        console.log(old, 'old');
-        var protocol = old[1] || 'https://';
+      var match = src.match(re);
+      if (match) {
+        var protocol = match[1] || 'https://';
+        var path = match[4] || '';
         var newDomain = actual.replace(/^https?:\/\//, '');
-        var path = old[4] || '';
         var newSrc = protocol + newDomain + path;
-        console.log(newSrc, 'newSrc');
-        if (f.src) {
-          f.src = newSrc;
+
+        if (frame.src) {
+          frame.src = newSrc;
         }
-        if (f.dataset && f.dataset.src) {
-          f.dataset.src = newSrc;
+        if (frame.dataset && frame.dataset.src) {
+          frame.dataset.src = newSrc;
         }
       }
     }
   }
 
   function replace() {
-    console.log('replace');
     var frames = document.body
       ? document.body.getElementsByTagName('iframe')
       : [];
     for (var i = 0; i < frames.length; i++) {
-      console.log(frames[i]);
       replaceFrameSrc(frames[i]);
     }
   }
 
   function init() {
-    console.log('init act');
     replace();
-
     var observer = new MutationObserver(function (mutations) {
       mutations.forEach(function (mutation) {
         mutation.addedNodes.forEach(function (node) {
           if (node.tagName === 'IFRAME') {
-            console.log(node, 'node iframe');
             replaceFrameSrc(node);
           }
         });
       });
     });
 
-    observer.observe(document.body, { childList: true, subtree: true });
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
   }
 
   if (document.readyState === 'loading') {
